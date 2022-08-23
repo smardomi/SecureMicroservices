91f169c3a6506923a975d74c8ec820a0a2a28fd8
Saeed Mardomi <saeedmardomi@hotmail.com>
2022-08-23 18:58:46 +0430

    Create Movie Cuud Actions and Use In-Memory Database

---
+++ src/Movies.API/Data/MoviesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Model;
+
+namespace Movies.API.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
--- src/Movies.API/Startup.cs
+++ src/Movies.API/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Movies.API.Data;
 
 namespace Movies.API
 {
@@ -26,6 +21,8 @@ namespace Movies.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MoviesContext>(options =>
+                   options.UseInMemoryDatabase("Movies"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
